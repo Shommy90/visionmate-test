c987ee71b56b2d26114700ff33b1e1c64a0df6a8
Milovan Marjanovic <marjanovic90@gmail.com>
2023-12-28 12:13:05 +0100

    add base_url const

--- src/api/helper-hook/users.ts
+++ src/api/helper-hook/users.ts
@@ -1,13 +1,18 @@
 import {useMemo} from 'react';
 import axios from 'axios';
-import {API_EXCLUDE_VALUES, SEED, USERS_NUMBER} from '../../constants/api';
+import {
+  API_EXCLUDE_VALUES,
+  BASE_URL,
+  SEED,
+  USERS_NUMBER,
+} from '../../constants/api';
 
 const useUsersHelper = () => {
   const helper = useMemo(() => {
     return {
       usersList: async () => {
         const response = await axios.get(
-          `https://randomuser.me/api/?results=${USERS_NUMBER}&seed=${SEED}&exc=${API_EXCLUDE_VALUES}`,
+          `${BASE_URL}?results=${USERS_NUMBER}&seed=${SEED}&exc=${API_EXCLUDE_VALUES}`,
         );
 
         return response;
