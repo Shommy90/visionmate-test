ea0094dcd905e28cdc5455a32e706292fcdcc194
Milovan Marjanovic <marjanovic90@gmail.com>
2023-12-28 11:49:02 +0100

    add api constants

--- src/api/helper-hook/users.ts
+++ src/api/helper-hook/users.ts
@@ -1,12 +1,13 @@
 import {useMemo} from 'react';
 import axios from 'axios';
+import {API_EXCLUDE_VALUES, SEED, USERS_NUMBER} from '../../constants/api';
 
 const useUsersHelper = () => {
   const helper = useMemo(() => {
     return {
       usersList: async () => {
         const response = await axios.get(
-          'https://randomuser.me/api/?results=200&seed=seed',
+          `https://randomuser.me/api/?results=${USERS_NUMBER}&seed=${SEED}&exc=${API_EXCLUDE_VALUES}`,
         );
 
         return response;
---
+++ src/constants/api.ts
@@ -0,0 +1,3 @@
+export const API_EXCLUDE_VALUES = 'cell,dob,nat,phone,registered,id';
+export const USERS_NUMBER = 200;
+export const SEED = 'seed';
